e87f6b9bb185fa6ad2caaaf6059e26e7a8672efb
Mohd Ridwan Roslan <mohammadridwan911@gmail.com>
2020-10-14 22:43:55 +0800

    roll over

--- index.php
+++ index.php
@@ -8,29 +8,35 @@
     <meta name="keywords" content="Visual Novel">
     <meta name="author" content="deathshadowworld">
     <title>Firehub Link</title>
-    <link rel="icon" href="../img/bonfire.gif">
+    <link rel="icon" href="../media/img/bonfire.gif">
 </head>
 
 
 <body>
 <header>
     <h1 class="mega">Hi</h1>
-    <p style="text-align: center; margin-top: 30px;">Welcome to the Hub of many things that I may or may not be interested with.</p>
+    <p style="text-align: center; margin-top: 30px; color: white;">Welcome to the Hub of many things that I may or may not be interested with.</p>
 </header>
 
 <nav>
     <ul class="navi">
-        <li><a href="test/home.html">Home 2</a></li><hr>
-        <li><a href="https://www.youtube.com/">Youtube</a></li><hr>
-        <li><a href="gurps.html">GURPS</a></li><hr>
-        <li><a href="vn.html">Web VN</a></li><hr>
-        <li><a href="japanese.html">日本語</a></li><hr>
-        <li><a href="manga.html">Manga</a></li><hr>
+        <li><a href="test/home.html">Home 2</a></li>
+        <li><a href="https://www.youtube.com/">Youtube</a></li>
+        <li><a href="gurps.html">GURPS</a></li>
+        <li><a href="vn.html">Web VN</a></li>
+        <li><a href="japanese.html">日本語</a></li>
+        <li><a href="manga.html">Manga</a></li>
         <li><a href="hentai.html">Lewd</a></li>
+        <li><a href="pictures.html">Pictures</a></li>
+        <li><a href="music/music.html">Music</a></li>
+        <li><a href="videos.html">Videos</a></li>
+        <li><a href="games.html">Games</a></li>
     </ul>
 </nav>
 
 
 
 </body>
+
+
 </html>
